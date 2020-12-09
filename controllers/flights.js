33f3118619e906f8aa6dfb123dd52d6fdc55fabe
VenkatNan <nanditavenkat@gmail.com>
2020-12-08 21:29:28 -0600

    its finals week and i am dying. i understand everything but idk why it doesnt wooooork

--- controllers/flights.js
+++ controllers/flights.js
@@ -7,8 +7,24 @@ module.exports = {
     create,
     index,
     show,
-    delete: deleteFlight,
-    addDestinations
+    // delete: deleteFlight,
+    // addDestinations
+}
+
+function index(req, res) {
+    Flight.find({}, (err, flights) => {
+        res.render('flights/index', { title: 'All Flights',flights: flights})
+    })
+}
+
+
+function show(req, res) {
+    Flight.findById(req.params.id)
+    .populate('destination').exec( (err, flight) => {
+        Destination.find({ _id: { $nin: flight.destinations } },(err, destinations) => {
+            res.render('flights/show', { title: 'Flight Details', flights })//destinations
+                })
+    })
 }
 
 function newFlight(req, res) {
@@ -22,37 +38,21 @@ function create(req, res) {
     const flight = new Flight(req.body)
     flight.save(function (err) {
         if (err) { return res.redirect('/flights/new') }
-        res.redirect(`/flights/${flight._id}`)
+        res.redirect(`/flights`)
     })
 }
 
-function index(req, res) {
-    Flight.find({}, (err, flights) => {
-        res.render('flights/index', { flights: flights, title: 'All Flights' })
-    })
-}
+// function deleteFlight(req, res) {
+//     Flight.findByIdAndDelete(req.params.id, (err, flight) => {
+//         res.redirect('/flights')
+//     })
+// }
 
-function show(req, res) {
-    Flight.findById(req.params.id)
-        .populate('destination').exec( (err, flight) => {
-            Destination.find({ _id: { $nin: flight.destinations } },
-                (err, destinations) => {
-                    res.render('flights/show', { title: 'Flight Details', flight })//destinations
-                })
-        })
-}
-
-function deleteFlight(req, res) {
-    Flight.findByIdAndDelete(req.params.id, (err, flight) => {
-        res.redirect('/flights')
-    })
-}
-
-function addDestinations(req, res) {
-    Flight.findById(req.params.id, (err, flight) => {
-        flight.destination.push(req.body.destination)
-        flight.save((err) => {
-            res.redirect(`/flights/${flight._id}`)
-        }) 
-    })
-}
+// function addDestinations(req, res) {
+//     Flight.findById(req.params.id, (err, flight) => {
+//         flight.destination.push(req.body.destination)
+//         flight.save((err) => {
+//             res.redirect(`/flights/${flight._id}`)
+//         }) 
+//     })
+// }
